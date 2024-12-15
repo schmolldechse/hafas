48b4a89f7371ff8777502f45c940a61814befd5e
Christian Knapp <schmolldechse@icloud.com>
2024-12-15 16:27:42 +0100

    add files

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import { createClient } from "hafas-client";
+import { profile } from "hafas-client/p/db/index.js";
+import { createHafasRestApi } from "hafas-rest-api";
+
+const config = {
+    hostname: "voldechse.wtf",
+    name: "Hafas-REST-API",
+    version: "1.0.0",
+    cors: false,
+    aboutPage: false
+}
+
+const PORT = 3000;
+
+const hafas = createClient(profile, "Hafas-REST-API");
+const api = await createHafasRestApi(hafas, config);
+
+api.listen(PORT, (err) => {
+    console.log(`REST-API is listening on ${PORT}`);
+    if (err) console.log(err);
+});
