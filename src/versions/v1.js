05390020a46cdf77c7c9669582e2165e16bec415
Christian Knapp <schmolldechse@icloud.com>
2024-12-24 11:18:18 +0100

    add cors logging

--- src/versions/v1.js
+++ src/versions/v1.js
@@ -5,17 +5,20 @@ import {createHafasRestApi} from "hafas-rest-api";
 const config = {
     hostname: "hafas-v1.voldechse.wtf",
     port: 15000,
-    name: "Hafas-REST",
+    name: "Voldechse Navigator",
     homePage: "https://hafas-v1.voldechse.wtf",
     version: "1.0.0",
     openapiSpec: true,
-    aboutPage: false,
-    cors: true
+    aboutPage: false
 }
 
-const hafas = createClient(profile, "wtf.HAFAS-REST", config);
+const hafas = createClient(profile, "HAFAS REST v1", config);
 const api = await createHafasRestApi(hafas, config);
 
+api.use((req, res, next) => {
+    res.on('finish', () => console.log('(v1) CORS headers:', res.getHeaders()['access-control-allow-origin']));
+    next();
+});
 api.listen(config.port, (err) => {
     console.log(`REST API (v1) is listening on ${config.port}`);
     if (err) console.log(err);
--- src/versions/v2.js
+++ src/versions/v2.js
@@ -5,17 +5,20 @@ import {createHafasRestApi} from "hafas-rest-api";
 const config = {
     hostname: "hafas-v2.voldechse.wtf",
     port: 15100,
-    name: "Hafas-REST",
-    homePage: "https://hafas-v2.voldechse.wtf",
+    name: "Voldechse Navigator",
+    homePage: "https://hafas-v1.voldechse.wtf",
     version: "2.0.0",
     openapiSpec: true,
-    aboutPage: false,
-    cors: true
+    aboutPage: false
 }
 
-const hafas = createClient(dbProfile, "wtf.HAFAS-REST", config);
+const hafas = createClient(dbProfile, "HAFAS REST v2", config);
 const api = await createHafasRestApi(hafas, config);
 
+api.use((req, res, next) => {
+    res.on('finish', () => console.log('(v2) CORS headers:', res.getHeaders()['access-control-allow-origin']));
+    next();
+});
 api.listen(config.port, (err) => {
     console.log(`REST API (v2) is listening on ${config.port}`);
     if (err) console.log(err);
